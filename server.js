6170df13999dad98d66c0bbe184365cd53a4394c
trentguillory <trentmichaelguillory@gmail.com>
2016-12-19 09:33:50 -0600

    command line IO from browser

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 'use strict';
 
+// Server setup
 const express = require('express');
 const socketIO = require('socket.io');
 const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
+
+// Require child process
+var spawn = require('child_process').spawn;
+
 
 // create HTTP server
 const server = express()
@@ -14,11 +19,38 @@ const server = express()
 
 const io = socketIO(server);
 
+// Start MC server manually
+var minecraftServerProcess = spawn('java', [
+    '-Xmx1024M',
+    '-Xms1024M',
+    '-jar',
+    'minecraft_server.1.11.jar',
+    'nogui'
+]);
+minecraftServerProcess.stdout.setEncoding('utf-8');
+minecraftServerProcess.stdout.setEncoding('utf-8');
+
+// log events from MC server to console
+function log(data) {
+    process.stdout.write(data.toString());
+}
+minecraftServerProcess.stdout.on('data', log);
+minecraftServerProcess.stderr.on('data', log);
+
 // Handle connections
 io.on('connection', (socket) => {
   console.log('Client connected');
-
   socket.on('disconnect', () => console.log('Client disconnected'));
+  socket.on('command', function(command) {
+    console.log("command: " + command);
+    minecraftServerProcess.stdin.write(command+'\n');
+  });
+});
 
-  socket.on('command', command => console.log("command: " + command));
+// Emit events from MC server to console
+minecraftServerProcess.stdout.on('data', function(data) {
+  io.emit('log', data + '\n');
+});
+minecraftServerProcess.stderr.on('data', function(data) {
+  io.emit('log', data + '\n');
 });
